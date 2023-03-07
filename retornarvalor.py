3c73e6fdbdcdf65090f75c182313298b9e6bddfc
CaioUnivale <estudos151927@gmail.com>
2023-03-06 21:55:21 -0300

    first commit

---
+++ retornarvalor.py
@@ -0,0 +1,9 @@
+valor1 = input("digite o valor 1")
+valor2 = input("digite o valor2")
+
+if valor1>valor2:
+  print('o seu maior valor foi o', valor1)
+elif valor2>valor1:
+  print('o seu maior valor foi o', valor2)
+else:
+  print("os valores são iguais")
